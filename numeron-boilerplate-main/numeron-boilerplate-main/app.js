02bc1836f304f555e3617c77c303faa6e81c1dba
Priyanshi-Rana <115335664+Priyanshi-Rana@users.noreply.github.com>
2022-10-18 17:06:18 +0530

    Add files via upload

---
+++ numeron-boilerplate-main/numeron-boilerplate-main/app.js
@@ -0,0 +1,9 @@
+// Iteration 1: Make the Play button functional.
+var playbutton = document.getElementById("play-button");
+
+
+// Description: When the Play button is clicked the game page should be displayed.
+
+playbutton.onclick= () => {
+    location.href="./game.html";
+}
